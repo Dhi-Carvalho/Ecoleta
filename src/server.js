1524eb331156ef23102f13e5a0586402a045c28a
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-02-18 18:26:24 -0300

    Server reorganization

--- src/server.js
+++ src/server.js
@@ -1,29 +1,25 @@
-const express = require("express")
-const server = express()
+function pageLanding(req,res) {
+    return res.render("index.html")
+}
 
-const db = require("./database/db")
-
-server.use(express.static("public"))
-
-server.use(express.urlencoded({ extended: true }))
-
-const nunjucks = require("nunjucks")
-nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
-
-server.get("/", (req, res) => {
-    return res.render("index.html", { title: "Um título"})
-})
-
-server.get("/create-point", (req, res) => {
-
+function createPoint(req,res) {
     return res.render("create-point.html")
-})
+}
 
-server.post("/savepoint", (req, res) => {
+function searchResults(req,res) {
+    const search = req.query.search
+    if(search == "") {
+        return res.render("search-results.html", {total: 0})}
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+            }
+        const total = rows.length
+        return res.render("search-results.html", { places: rows, total: total})
+    })
+}
 
+function savePoint(req,res) {
     const query = `
         INSERT INTO places (
             image,
@@ -58,21 +54,27 @@ server.post("/savepoint", (req, res) => {
 
     db.run(query, values, afterInsertData)
 
+}
+
+const express = require("express")
+const server = express()
+
+const db = require("./database/db")
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
 })
 
-server.get("/search", (req, res) => {
-    const search = req.query.search
-    if(search == "") {
-        return res.render("search-results.html", { total: 0})
-    }
+server
+.use(express.static("public"))
+.use(express.urlencoded({ extended: true }))
 
-    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
-        if(err) {
-            return console.log(err)
-        }
-        const total = rows.length
-        return res.render("search-results.html", { places: rows, total: total})
-    })
-})
+.get("/", pageLanding)
+.get("/create-point", createPoint)
+.get("/search", searchResults)
 
-server.listen(3000)
+.post("/savepoint", savePoint)
+
+.listen(3000)
